a63a8595d318a0cd1716320ff8cded8aa2d2ed00
Yohannes91 <63756368+Yohannes91@users.noreply.github.com>
2021-08-07 13:46:46 -0400

    pretest assignemnets

--- day1/hello.py
+++
@@ -1,5 +0,0 @@
-first_name = 'Jojo' 
-last_name = 'tec'
-full_name = first_name + " " + last_name
-
-print (full_name)
--- day1/stats.py
+++
@@ -1,5 +0,0 @@
-data = [20,19,25,35,18]
-
-mean = sum(data)/len(data)
-print ("The mean of the students age is:  ", mean)
- 
